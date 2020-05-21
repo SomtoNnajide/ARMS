2b11d382075a94026bc2282d49c1e1bcea9d5520
SomtoNnajide <64958440+SomtoNnajide@users.noreply.github.com>
2020-05-21 14:52:23 +1000

    3rd buzzer test to make buzzer beep.
    3rd test whereby the buzzer beeps at a single frequency every second.
    Buzzer beep experimentation complete.

---
+++ beep.py
@@ -0,0 +1,19 @@
+#3rd buzzer test
+#the aim is to make buzzer beep every second at 261 frequency
+
+#Libraries
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM) #pinmode
+GPIO.setup(18,GPIO.OUT) #pin output
+pwm = GPIO.PWM(18,261) #set pwm pin and frequency
+
+for i in range(10):
+    pwm.start(70) #beep algorithm
+    time.sleep(1)
+    pwm.start(0)
+    time.sleep(1)
+
+GPIO.cleanup() #resets pin
+
