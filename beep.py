1bb7ec1f83319c43b3d341514aa215923ecfd92d
SomtoNnajide <64958440+SomtoNnajide@users.noreply.github.com>
2020-06-14 23:21:58 +1000

    3rd buzzer test to make buzzer beep.

--- beep.py
+++ beep.py
@@ -9,7 +9,7 @@ GPIO.setmode(GPIO.BCM) #pinmode
 GPIO.setup(18,GPIO.OUT) #pin output
 pwm = GPIO.PWM(18,261) #set pwm pin and frequency
 
-for i in range(10):
+for i in range(5):
     pwm.start(70) #beep algorithm
     time.sleep(1)
     pwm.start(0)
