77f5454a3a49b2452e5ac93d767714f9e36e2931
SomtoNnajide <64958440+SomtoNnajide@users.noreply.github.com>
2020-06-14 11:01:54 +1000

    Update buzzcam.py
    Added comments to make code interpretation easier.

--- buzzcam.py
+++ buzzcam.py
@@ -1,23 +1,30 @@
+#Libraries
 from picamera import PiCamera
 from time import sleep
 import RPi.GPIO as GPIO
 import os.path
 import time
 
-camera = PiCamera()
-picpath = '/home/pi/Desktop/test.jpg'
+camera = PiCamera() #instantiate camera
+picpath = '/home/pi/Desktop/test.jpg' #define picture path
 
+#setup pi board and pwm pin
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
 pwm = GPIO.PWM(18, 261)
 
-camera.start_preview()
+#take picture and save to already defined path
+camera.start_preview() #start camera preview
 sleep(5)
 camera.capture(picpath)
-camera.stop_preview()
+camera.stop_preview() #close camera preview
 
+#a test to check if the path exists 
+#buzzer beeps once if true
+#and three times if false
+#similar to later on system functionality whereby it beeps if barcode is recognised or not
 def findPic(path):
-    if os.path.exists(path):
+    if os.path.exists(path): #check path
         pwm.start(50)
         time.sleep(1)
         pwm.start(0)
@@ -29,5 +36,5 @@ def findPic(path):
             pwm.start(0)
             time.sleep(0.3)
 
-if __name__ == "__main__":
-    findPic(picpath)
+if __name__ == "__main__": #python main function
+    findPic(picpath) #call function with parameter
